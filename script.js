3eb7a1049703c882811077edd483c37acdbaa1d6
plaughlin98 <plaughlin98@gmail.com>
2020-11-19 12:05:38 -0600

    Formatting Project Popups

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function showDropdown() {
 }
 
 window.onclick = function(e) {
-    if (!e.target.matches('.nav-btn')) {
+    if (!e.target.matches('.nav-btn') && !e.target.matches('.spinny')) {
         var dropdown = document.getElementsByClassName('nav-content');
         for (let i = 0; i < dropdown.length; i++) {
             if(dropdown[i].classList.contains('show')) {
@@ -17,7 +17,9 @@ window.onclick = function(e) {
 }
 
 function showSlide() {
-    console.log(document.getElementById('title-buttons-column'));
+    var projects = document.getElementById("popup-projects")
+    projects.classList.toggle('show-flex')
+    projects.classList.toggle('hide-flex')
     document.getElementById("title-area").classList.toggle('slide-left');
     document.getElementById("title-buttons-column").classList.toggle('title-btn');
 }
